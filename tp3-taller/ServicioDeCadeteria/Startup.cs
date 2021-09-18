9437db4ee0657a95a1869b7b07bea2ab4599cded
DerAufhocker <exequieldiaz404@gmail.com>
2021-09-18 04:15:45 -0300

    Creacion de un nuevo proyecto

--- tp3-taller/ServicioDeCadeteria/Startup.cs
+++ tp3-taller/ServicioDeCadeteria/Startup.cs
@@ -8,14 +8,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using tp3_taller.Entidades;
 
-namespace tp3_taller
+namespace ServicioDeCadeteria
 {
     public class Startup
     {
-
-        static List<Cadete> ListaCadetes = new List<Cadete>();
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,14 +23,7 @@ namespace tp3_taller
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddSingleton(ListaCadetes);
-
-            
-            services.AddSingleton(ListaCadetes);
-
-
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- tp3-taller/tp3-taller/Controllers/CadeteController.cs
+++ tp3-taller/tp3-taller/Controllers/CadeteController.cs
@@ -1,67 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using tp3_taller.Entidades;
-
 namespace tp3_taller.Controllers
 {
     public class CadeteController : Controller
     {
-        static int id = 0;
-<<<<<<< HEAD
-
         private readonly ILogger<CadeteController> _logger;
-        private readonly List<Cadete> cadetes;
-        public CadeteController(ILogger<CadeteController> logger, List<Cadete>Cadetes)
-=======
-        
 
-        private readonly ILogger<CadeteController> _logger;
-        private readonly List<Cadete> cadetes;
-
-        public CadeteController(ILogger<CadeteController> logger,List<Cadete> Cadetes)
->>>>>>> d27da61527637817c8ce9033175b63001d05ea5e
+        public HomeController(ILogger<HomeController> logger)
         {
-            cadetes = Cadetes;
             _logger = logger;
-            cadetes = Cadetes;
             _logger.LogDebug(1, "NLog injected into HomeController");
         }
         public IActionResult Index()
         {
-            return View(cadetes);
-        }
-
-        public IActionResult AltaCadete(string nombre, string direccion,string telefono)
-        {
-            try
-            {
-<<<<<<< HEAD
-                id++;
-                Cadete MiCadete = new Cadete(nombre, direccion, telefono);
-                MiCadete.AgregarID(id);
-                cadetes.Add(MiCadete);
-
-=======
-                if (nombre != null)
-                {
-                    id++;
-                    Cadete MiCadete = new Cadete(nombre, direccion, telefono);
-                    MiCadete.AgregarID(id);
-                    cadetes.Add(MiCadete);
-                }
->>>>>>> d27da61527637817c8ce9033175b63001d05ea5e
-                return View();
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
+            return View();
         }
     }
 }
